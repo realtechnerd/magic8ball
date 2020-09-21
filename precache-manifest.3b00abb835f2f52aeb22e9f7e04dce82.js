441cc610d41ff016b4103e85b41713cc5f717f96
realtechnerd <40891074+realtechnerd@users.noreply.github.com>
2020-09-20 20:20:36 -0400

    Updates

---
+++ precache-manifest.3b00abb835f2f52aeb22e9f7e04dce82.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "267e3a7f8102282f00194f4e7d6b3094",
+    "url": "/magic8ball/index.html"
+  },
+  {
+    "revision": "104c83b5dfe392708547",
+    "url": "/magic8ball/static/css/main.97c491c5.chunk.css"
+  },
+  {
+    "revision": "6bf006113e0d94ec9991",
+    "url": "/magic8ball/static/js/2.ceac5083.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/magic8ball/static/js/2.ceac5083.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "104c83b5dfe392708547",
+    "url": "/magic8ball/static/js/main.a1d8a3df.chunk.js"
+  },
+  {
+    "revision": "1f8489669057e4d9546a",
+    "url": "/magic8ball/static/js/runtime-main.0ffb8d2d.js"
+  }
+]);
